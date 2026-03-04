034b8c703ddfbd9dc01dd375bb487607b5255d2e
Benjamin Cahill <benjamin.a.cahill2@gmail.com>
2026-03-04 08:43:01 -0500

    Slime Enemy
    worked on a basic slime enemy

---
+++ Gnome Game/Assets/Scripts/Enemy/StateMachines/EnemyStateMachine.cs
@@ -0,0 +1,20 @@
+  using UnityEngine;
+
+public class EnemyStateMachine
+{
+    public EnemyState CurrentState { get; private set; }
+
+    public void Initialize(EnemyState startingState)
+    {
+        CurrentState = startingState;
+        CurrentState.EnterState();
+    }
+
+    public void ChangeState(EnemyState newState)
+    {
+        CurrentState.ExitState();
+        CurrentState = newState;
+        CurrentState.EnterState();
+    }
+
+}
