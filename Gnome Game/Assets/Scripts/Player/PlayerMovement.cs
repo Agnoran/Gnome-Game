f62f9226c3c7db3d39db2035e5a15cd223338dbf
Miguel Flores <miguelflores2691@gmail.com>
2026-03-15 20:20:19 -0400

    Revert "Merge branch 'Ben-Dev' into Miguel-Dev"
    This reverts commit df4e5d8aca7d9802e90650af9ca1a9b36fb285da, reversing
    changes made to ec56e9dd432758ea141dd376f47bd0ecaba9addc.

--- Gnome Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Gnome Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,93 +1,60 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement")]
     [SerializeField] float moveSpeed = 5f;
+    float origMoveSpeed;
     [SerializeField] float sprintMultiplier = 1.5f;
+    float origSprintMod;
 
-    [Header("Jump / Gravity")]
     [SerializeField] float gravity = 9.81f;
-    [SerializeField] float jumpForce = 1.5f;
+    [SerializeField] float jumpforce = 1.5f;
 
-    [Header("Rotation")]
-    [SerializeField] float rotateSpeed = 15f;
-    [SerializeField] Transform visualRoot;
+    [SerializeField] float rotateSensitivity;
 
-    [Header("References")]
     [SerializeField] CharacterController playerController;
     [SerializeField] Camera mainCamera;
     [SerializeField] PlayerInputHandler inputHandler;
 
-    [Header("Options")]
     [SerializeField] bool isToggleSprint = false;
+    bool isSprinting = false;
+
 
     Vector3 currentMovement;
+    Vector3 verticalRotation;
 
     void Awake()
     {
-        if (playerController == null)
-        {
-            playerController = GetComponent<CharacterController>();
-        }
-
-        if (mainCamera == null)
-        {
-            mainCamera = GetComponentInChildren<Camera>();
-
-            if (mainCamera == null)
-            {
-                mainCamera = Camera.main;
-            }
-        }
-
-        if (inputHandler == null)
-        {
-            inputHandler = PlayerInputHandler.Instance;
-
-            if (inputHandler == null)
-            {
-                inputHandler = FindFirstObjectByType<PlayerInputHandler>();
-            }
-        }
+        playerController = GetComponent<CharacterController>();
+        inputHandler = PlayerInputHandler.Instance;
+        origMoveSpeed = moveSpeed;
+        origSprintMod = sprintMultiplier;
     }
-
+    // Update is called once per frame
     void Update()
     {
+
         HandleMovement();
-        HandleRotationToMouse();
+        HandleRotation();
     }
 
     void HandleMovement()
     {
-        if (playerController == null || mainCamera == null || inputHandler == null)
-        {
-            return;
-        }
-
+       
         bool isSprinting = inputHandler.SprintValue > 0.1f;
-
         if (isToggleSprint)
         {
             isSprinting = inputHandler.SprintToggleValue;
         }
-
+        
         float currentSpeed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
 
-        Vector2 moveInput = inputHandler.MoveInput;
+        Vector3 inputDirection = new Vector3(inputHandler.MoveInput.x, 0f, inputHandler.MoveInput.y);
+        inputDirection = Vector3.ClampMagnitude(inputDirection, 1f);
 
-        Vector3 cameraForward = mainCamera.transform.forward;
-        Vector3 cameraRight = mainCamera.transform.right;
-
-        cameraForward.y = 0f;
-        cameraRight.y = 0f;
-
-        cameraForward.Normalize();
-        cameraRight.Normalize();
-
-        Vector3 moveDirection = (cameraForward * moveInput.y) + (cameraRight * moveInput.x);
-        moveDirection = Vector3.ClampMagnitude(moveDirection, 1f);
+        Vector3 moveDirection = transform.TransformDirection(inputDirection);
 
         currentMovement.x = moveDirection.x * currentSpeed;
         currentMovement.z = moveDirection.z * currentSpeed;
@@ -101,17 +68,31 @@ public class PlayerMovement : MonoBehaviour
     {
         if (playerController.isGrounded)
         {
-            currentMovement.y = -0.5f;
+            currentMovement.y = -0.5f; // Small downward force to keep the player grounded
 
             if (inputHandler.JumpInput)
             {
-                currentMovement.y = jumpForce;
+                currentMovement.y = jumpforce;
             }
         }
         else
         {
             currentMovement.y -= gravity * Time.deltaTime;
         }
+
+    }
+    void OnSprint(InputAction Context)
+    {
+
+    }
+    void HandleRotation()
+    {
+        float rotateDir = inputHandler.RotateCameraInput * rotateSensitivity;
+        transform.Rotate(0, rotateDir, 0);
+    }
+    void ModSpeed(float amount)
+    {
+        moveSpeed += amount;
     }
     public void hasteMoveSpeed(float amount)
     {
@@ -128,52 +109,5 @@ public class PlayerMovement : MonoBehaviour
     public void moveSpeedReset()
     {
         moveSpeed = origMoveSpeed;
-    }
-}
-
-    void HandleRotationToMouse()
-    {
-        if (mainCamera == null || Mouse.current == null || visualRoot == null)
-        {
-            return;
-        }
-
-        Vector3 playerScreenPosition = mainCamera.WorldToScreenPoint(transform.position);
-        Vector2 mouseScreenPosition = Mouse.current.position.ReadValue();
-
-        Vector2 screenLookDirection = mouseScreenPosition - new Vector2(playerScreenPosition.x, playerScreenPosition.y);
-
-        if (screenLookDirection.sqrMagnitude < 0.001f)
-        {
-            return;
-        }
-
-        screenLookDirection.Normalize();
-
-        Vector3 cameraForward = mainCamera.transform.forward;
-        Vector3 cameraRight = mainCamera.transform.right;
-
-        cameraForward.y = 0f;
-        cameraRight.y = 0f;
-
-        cameraForward.Normalize();
-        cameraRight.Normalize();
-
-        Vector3 worldLookDirection =
-            (cameraRight * screenLookDirection.x) +
-            (cameraForward * screenLookDirection.y);
-
-        if (worldLookDirection.sqrMagnitude < 0.001f)
-        {
-            return;
-        }
-
-        Quaternion targetRotation = Quaternion.LookRotation(worldLookDirection);
-
-        visualRoot.rotation = Quaternion.Slerp(
-            visualRoot.rotation,
-            targetRotation,
-            rotateSpeed * Time.deltaTime
-        );
     }
 }
