958487f43cc45bac58d1de458f6a708a22055e36
Miguel Flores <miguelflores2691@gmail.com>
2026-03-17 22:12:56 -0400

    goal trigger

---
+++ Gnome Game/Assets/Scripts/goalTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class goalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            WorldController.instance.StateWinGame();
+        }
+    }
+}
