b48e438baadf5ee63721d4ba478a7059ffd12305
MSSommers <1981insane@gmail.com>
2026-03-22 18:01:01 -0400

    Fixed All Menus And Functions

--- Gnome Game/Assets/Scripts/Game Settings/settingsManager.cs
+++ Gnome Game/Assets/Scripts/Game Settings/settingsManager.cs
@@ -1,23 +1,30 @@
-using UnityEngine;
+using System.Collections.Generic;
 using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class settingsManager : MonoBehaviour
 {
-    public Slider volumeSlider;
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
     public TMP_Dropdown resolutionDropdown;
     public Toggle fullscreenToggle;
 
     Resolution[] resolutions;
 
-    void Start()
+    private void OnEnable()
     {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
 
+        resolutions = Screen.resolutions;
+        //resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
         int currentResolutionIndex = 0;
 
-        var options = new System.Collections.Generic.List<string>();
+        //var options = new System.Collections.Generic.List<string>();
 
         for (int i = 0; i < resolutions.Length; i++)
         {
@@ -33,30 +40,65 @@ public class settingsManager : MonoBehaviour
             }
         }
 
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+        SetResolution(currentResolutionIndex);
+        //resolutionDropdown.AddOptions(options);
 
+        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
+       // resolutionDropdown.value = currentResolutionIndex;
+       // resolutionDropdown.RefreshShownValue();
+
+        SetFullscreen(fullscreenToggle);
         fullscreenToggle.isOn = Screen.fullScreen;
+      
+
+        // Load saved values
+        //musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        //sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+
+        //SetMusicVolume(musicSlider.value);
+        //SetSFXVolume(sfxSlider.value);
+
+        // Remove old listeners
+        //musicSlider.onValueChanged.RemoveAllListeners();
+        //sfxSlider.onValueChanged.RemoveAllListeners();
+        // Add listeners
+        //musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        //sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        PlayerPrefs.Save();
     }
 
-    public void SetVolume(float volume)
+    public void SetMusicVolume(float value)
     {
-        AudioListener.volume = volume;
+        float volume = Mathf.Log10(Mathf.Max(value, 0.0001f)) * 20;
+        audioMixer.SetFloat("MusicVolume", volume);
+
+        PlayerPrefs.SetFloat("MusicVolume", value);
     }
 
+    public void SetSFXVolume(float value)
+    {
+        float volume = Mathf.Log10(Mathf.Max(value, 0.0001f)) * 20;
+        audioMixer.SetFloat("SFXVolume", volume);
+
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+    
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
 
-        Screen.SetResolution(
-            resolution.width,
+        Screen.SetResolution
+            (resolution.width,
             resolution.height,
             Screen.fullScreen);
+        PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
+        PlayerPrefs.Save();
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
-    }
+        PlayerPrefs.SetInt("Fullscreen", Screen.fullScreen ? 1 : 0);
+        PlayerPrefs.Save();
+    } 
 }
