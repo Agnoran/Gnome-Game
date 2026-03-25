5802cfe144b56ccc7efdfed2a6468df8a7e2158a
Velma187 <143227625+Velma187@users.noreply.github.com>
2026-03-25 15:04:07 -0400

    made it so that it transitions to next scene in the build list

--- Gnome Game/Assets/Scripts/StartMenuController.cs
+++ Gnome Game/Assets/Scripts/StartMenuController.cs
@@ -6,39 +6,22 @@ public class StartMenuController : MonoBehaviour
     [SerializeField] Animator gnomeAnimator;
     [SerializeField] string animationName = "Take 001";
 
-    void Start()
-    {
-        // GIVE UNITY A BREATHER: Wait 0.1 seconds for the scene to settle
-        Invoke("BootSequence", 0.1f);
-    }
-
-    void BootSequence()
-    {
-        // 1. ANIMATION FIX
-        if (gnomeAnimator != null)
-        {
-            gnomeAnimator.enabled = true;
-            gnomeAnimator.Play(animationName, 0, 0f);
-            Debug.Log("Miu, I am playing: " + animationName);
-        }
-        else
-        {
-            Debug.LogError("Miu, the Gnome Animator slot is EMPTY!");
-        }
-
-        // 2. MUSIC FIX (Ensures music starts even if SceneManager missed it)
-        if (AudioManager.instance != null)
-        {
-            AudioManager.instance.Play("Music_Start Menu");
-        }
-    }
-
     public void StartGame()
     {
         if (AudioManager.instance != null)
             AudioManager.instance.Play("Transition");
 
-        SceneManager.LoadScene("Beta Milestone Scene");
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogError("There is no scene after this one in the Build Settings!");
+        }
     }
 
     public void QuitGame()
