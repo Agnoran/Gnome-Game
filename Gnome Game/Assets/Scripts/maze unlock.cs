0849aa2742dc9e900546ed4554abce0d68ee7592
Benjamin Cahill <benjamin.a.cahill2@gmail.com>
2026-03-29 23:33:51 -0400

    admin

--- Gnome Game/Assets/Scripts/maze unlock.cs
+++ Gnome Game/Assets/Scripts/maze unlock.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class mazeunlock : MonoBehaviour
 {
     [SerializeField] GameButton unlockButtton;
-    
+    [SerializeField] int requiredGnomes = 4;
 
     // Update is called once per frame
     void Update()
     {
-        if(HUD.instance.GetGnomes() >= 4)
+        if(HUD.instance.GetGnomes() >= requiredGnomes)
         {
             Debug.Log("unlocking maze");
             Destroy(gameObject);
